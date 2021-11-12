4a13e26912aad972bb8431e4e4516a2ac9eeb091
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2021-11-12 19:00:03 -0300

    Delete atividade V/codigo directory

--- atividade V/codigo/resources/views/entradas/index.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>paginas de entradas</h1>
-
-@foreach ($Entradas as $entrada)
-    <ul>
-        <li>valor da entrada {{$entrada->valortotaldanota}} </li>
-        <li>data da entrada {{$entrada->dataentrada}}</li>
-    </ul>
-
-@endforeach

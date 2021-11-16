e91c84f1e5dfdf3c1105c0f9a8d52a09e5c6a57e
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2021-11-16 16:13:19 -0300

    Add files via upload

---
+++ codigo/views/categoria/index.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.main')
+
+@section('titulo','Sistema de Venda')
+
+
+@section('conteudo')
+
+  <table class="table table-sm">
+    <div>
+        <caption>Tabela de categoria</caption>
+       
+    </div>
+   
+      @foreach ($categorias as $categoria)
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th>nome</th><br>
+          <th></th>
+          <th></th>
+          <th></th>
+
+        </tr>
+      </thead>
+       
+            <td scope="row">{{$categoria->id}}</td>
+
+          <td>{{$categoria->nome}}</td>
+          
+             <td>
+                   <form action="{{route('categoria.delete', ['id' => $categoria->id])}}" method="post">
+                    @csrf
+                    @method('DELETE')   
+                    <input type="submit" class="btn btn-primary" value="deletar">
+                    </form> 
+                </td>
+                <td>
+                    <form action="{{route('categoria.create')}}" method="post">
+                        @csrf
+                        <input type="submit" class="btn btn-primary" name="formulario" value="novo">
+                    </form>
+                </td>
+                <td>
+                    <form action="{{route('categoria.edit', ['id' => $categoria->id])}}" method="post">
+                        @csrf
+                        <input type="submit" class="btn btn-primary" name="formulario" value="alterar">
+                    </form>
+                </td>
+          
+          @endforeach
+     
+     
+    </table>
+
+
+    
+
+
+@endsection('conteudo')
+
+@section('footer')
+
+@endsection('footer')
+
+
+
+
---
+++ codigo/views/clientes/create.blade.php
@@ -0,0 +1,70 @@
+
+@extends('layouts.main')
+
+@section('titulo','Sistema de Venda')
+
+
+@section('conteudo')
+    <form action="{{route('cliente.store')}}" method="post">
+        <div class="row g-3">
+
+             <h4 align="center">Cadastro de cliente</h4><br>
+                 
+                <div class="col">
+
+                    @csrf
+                            
+                    <label for="nome">nome</label>
+                    <input type="text" class="form-control" name="nome" id="nome">
+
+                    <label for="debito">debito</label>
+                    <input type="text" class="form-control" name="debito" id="debito">
+
+                    <label for="cpf">cpf</label>
+                    <input type="text" class="form-control" name="cpf" id="cpf"><br><br>
+
+                    <input type="submit" class="btn btn-primary" value="cadastrar">    
+                        
+                </div><br>
+
+                <div class="col">
+                    <label for="rg">rg</label>
+                    <input type="text" class="form-control" name="rg" id="rg">
+
+
+                    <label for="datanascimento">datanascimento</label>
+                    <input type="date" class="form-control" name="datanascimento" id="datanascimento">
+
+                    <label for="sexo">sexo</label>
+                    <input type="text" class="form-control" name="sexo" id="sexo"><br><br>
+
+                           
+                    
+                </div>
+
+                 <div class="col">
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                       
+                            @foreach($errors->all() as $error)
+
+                                <label>{{$error}}</label><br>
+                            @endforeach
+                        
+                        </div>
+
+                    @endif
+            
+                 </div>
+
+    </div>
+
+    </form>
+
+                
+
+@endsection('conteudo')
+
+@section('footer')
+
+@endsection('footer')

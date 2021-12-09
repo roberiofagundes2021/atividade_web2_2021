baa8db0db35024fff310277200d80476a469f898
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2021-12-08 22:23:50 -0300

    Add files via upload

---
+++ Projeto-II/views/layouts/index.blade.php
@@ -0,0 +1,62 @@
+
+
+@section('titulo','Perfume-Shop')
+@section('conteudo')
+<div>
+
+
+</div>
+
+<table class="table table-sm">
+    <div>
+             <h4>Tabela de Fornecedor</h4>
+        </div>
+@foreach ($Fornecedors as $Fornecedor)
+
+
+    <thead>
+        <th>nome</th>
+        <th>cpnj</th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </thead>
+</div>
+    <thead>
+        <td> {{$Fornecedor->razaosocial}}</td>
+        <td>{{$Fornecedor->cnpj}}</td>
+
+         <td>
+            <form action="{{route('fornecedor.create')}}" method="post">
+                @csrf
+                <input type="submit" class="btn btn-primary" name="formulario" value="novo">
+            </form>
+         </td>
+
+          <td>
+            <form action="{{route('fornecedors.edit', ['id' => $Fornecedor->id])}}" method="post">
+                @csrf
+                <input type="submit" class="btn btn-primary" name="formulario" value="alterar">
+            </form>
+         </td>
+
+         <td>
+            <form action="{{route('fornecedors.delete', ['id' => $Fornecedor->id])}}" method="post">
+                @csrf
+                @method('DELETE')
+                <div id="button">
+                <input type="submit" class="btn btn-primary" name="formulario"  value="deletar">
+                </div>
+
+            </form>
+         </td>
+    </thead>
+
+@endforeach
+    </div>
+
+</table>
+@endsection('conteudo')
+@section('footer')
+
+@endsection('footer')

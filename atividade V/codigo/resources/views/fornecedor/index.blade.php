d54fac46647181539d1a58c5248ed49c2b0adf64
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-02-02 20:21:32 -0300

    Delete atividade V/codigo directory

--- atividade V/codigo/resources/views/fornecedor/index.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>pagina de fornecedores<h1>
-
-@foreach ($Fornecedors as $Fornecedor)
-    <ul>
-        <li>nome: {{$Fornecedor->nome}}</li>
-        <li>CNPJ: {{$Fornecedor->CNPJ}}</li>
-    </ul>
-@endforeach
